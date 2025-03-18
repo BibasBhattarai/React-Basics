ede129436a60fb54d48771e527084f1d643a1498
BibasBhattarai <bibashbhattarai720@gmail.com>
2025-03-18 21:30:16 +0545

    useState basic

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import React from "react"
-import Card from "./Components/Card";
+import React, { useState } from "react"
+// import Card from "./Components/Card";
 
 function App(){
 
-  const data=["messi", "suarez", "Neymar"]
+ const [banned,setbanned]=useState(false)
+  // const data=["messi", "suarez", "Neymar"]
   return (
-    <div>
-      <Card />
+    <div className="px-5 py-5">
+      {/* <Card /> */}
+      <h1>{banned.toString()}</h1>
+      <button onClick={()=>{setbanned(!banned)}} className="px-2 py-1 bg-blue-700 rounded-full mt-2">change the score</button>
     </div>
     
   )
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -3,41 +3,27 @@ import React from 'react'
 function Card() {
 
   const data=[
-    {image:'https://plus.unsplash.com/premium_photo-1679513691474-73102089c117?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aGVhZHBob25lfGVufDB8fDB8fHww', name:"Headphone", description:"Good quality headphones" , instock:false},
-    {image:'https://plus.unsplash.com/premium_photo-1681302427948-2fd0eca629b1?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8bGFwdG9wfGVufDB8fDB8fHww', name:"Laptop", description:"High performance Laptop ",instock:true},
-    {image:'https://images.unsplash.com/photo-1622434641406-a158123450f9?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHdhdGNofGVufDB8fDB8fHww', name:"Watch", description:"Water Resistance watch", instock:false},
-    {image:'https://images.unsplash.com/photo-1528795259021-d8c86e14354c?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fG1vYmlsZXxlbnwwfHwwfHx8MA%3D%3D', name:"I-phone", description:"I-phone with good quality Camera",instock:true}
-  ]
+    {name:"diamond and Rust", description:"This song is sang by Joan baez in 1973"},
+    {name:"November rain", description:"This song is sang by Guns and Roses in 1997"},
+   
 
+  ]
+    const handledownload=()=>{alert("you can't download")}
 
   return (
-    <div className='w-full h-screen flex items-center justify-center bg-zinc-200 gap-10'>
-
-      {data.map((elem,index)=>(<div key={index} className='w-52 bg-zinc-100 rounded-md overflow-hidden'>
-        <div className='w-full h-32 bg-zinc-300'>
-          <img className='w-full h-full object-cover' src={elem.image} alt="" />
-        </div>
-        <div className='w-full px-3 py-4'>
-          <h2 className='font-semibold'>{elem.name}</h2>
-          <p className='text-xs mt-3'>{elem.description}</p>
-          <button className={`px-4 py-1 ${elem.instock ?"bg-blue-600":"bg-red-700"}  text-xs rounded text-zinc-100 mt-3 w-fit` }>
-          {elem.instock ? "Instock":"Out of stock"}
-          </button>
-          
-        </div>
-      </div>))}
-
-      
-
-      
-      
-    </div>
     
+      <div className='w-full h-screen bg-zinc-300 justify-center items-center flex flex-col gap-4'>
+        {data.map((items, index)=><div className="w-100 song px-3 py-2 bg-zinc-100 rounded-md">
+          <h3 className='font-semibold text-xl'>{items.name}</h3>
+          <p className='text-xs mt-2'>{items.description}</p>
+          <button onClick={handledownload} className='px-2 py-1 bg-blue-400 text-xs font-semibold text-zinc-800 rounded-md mt-3'> Download</button>
+        </div>)}
+        
+      </div>
     
-
-
-
+      
   )
 }
 
 export default Card
+
