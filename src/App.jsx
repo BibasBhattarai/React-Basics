49bc8b733019846844fa24efeb3c5cd3b872666b
BibasBhattarai <bibashbhattarai720@gmail.com>
2025-03-20 23:37:33 +0545

    learning continue

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,280 @@
-import React, { useState } from "react"
-// import Card from "./Components/Card";
+// import React, { useState } from "react"
+// // import Card from "./Components/Card";
 
-function App(){
+// function App(){
 
- const [banned,setbanned]=useState(false)
-  // const data=["messi", "suarez", "Neymar"]
+//  const [banned,setbanned]=useState(false)
+//   // const data=["messi", "suarez", "Neymar"]
+//   return (
+//     <div className="px-5 py-5">
+//       {/* <Card /> */}
+//       <h1>{banned.toString()}</h1>
+//       <button onClick={()=>{setbanned(!banned)}} className="px-2 py-1 bg-blue-700 rounded-full mt-2">change the score</button>
+//     </div>
+    
+//   )
+// }
+
+// export default App;
+
+
+
+
+// second example of useState
+// import React, { useState } from 'react'
+
+// function App() {
+
+//   const [value,setvalue]=useState(15)
+
+
+//   return (
+//     <div className='p-5'>
+//       <h1>{value}</h1>
+//       <button onClick={()=>setvalue((prev)=>prev+1)} className='px-2 py-1 bg-blue-500  rounded-full'>click and change</button>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+
+// Third example of useState
+
+// import React, { useState } from 'react'
+
+// function App() {
+  
+//   const[val, setval]=useState({name:"yamal", isbanned:false})
+//   return (
+//     <div className='p-6'>
+//       <h1>name :{val.name}</h1>
+//       <h2>isbanned:{val.isbanned.toString()}</h2>
+//       <button onClick={()=>setval({...val,isbanned:!val.isbanned})} className={`px-2 py-1 ${val.isbanned ? "bg-blue-500" :"bg-red-500"} rounded-full`}>Change</button>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+
+// fouth example of usestate 
+
+// import React from 'react'
+// import { useState } from 'react'
+
+// function App() {
+  
+//   const [val,setval]=useState({name:"bibas", age:23})
+  
+//   return (
+  
+//     <div className='p-5'>
+//       <button onClick={()=>{
+//         setval({...val,gender:"male"})
+//         console.log(val);
+        
+        
+//         }} 
+//         className='px-2 py-1 bg-blue-500'>Add</button>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+
+// fifth example of usestate with array
+
+// import React from 'react'
+// import { useState } from 'react'
+
+// function App() {
+  
+//   const[val,setval]=useState([1,2,3,4,5,6,7,8,9])
+//   return (
+//     <div  className='p-6'>
+//     {val.map(items=><h1>{items}</h1>)}
+//     <button onClick={()=>setval(()=>{
+//      return val.filter((items,index)=>index!=val.length-1)
+
+
+
+//     })} className='px-2 py-1 bg-blue-500 rounded-full'>remove</button>
+      
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+
+//# sixth example of usestate with arrays
+
+// import React from 'react'
+// import { useState } from 'react'
+
+// function App() {
+  
+//   const [val, setval]=useState([1,2,3,4,5,6])
+  
+//   return (
+//     <div className='p-5'>
+//       {val.map(items=> <h1>{items}</h1>)}
+//       <button onClick={()=>setval(()=>val.filter(items=>items%2!==0))} className='px-1 py-1 bg-green-500 rounded-md '>click</button>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+
+// # seventh example of usestate with arrays 
+
+// import React from 'react'
+// import { useState } from 'react'
+
+// function App() {
+//   const [val,setval]=useState([1,2,3,4,5,6,7])
+
+  
+  
+//   return (
+//     <div>
+//       {val.map(items=><h1>{items}</h1>)}
+//       <button onClick={()=>setval([...val,8])} className='px-2 py-1 bg-blue-500 rounded-md' > click
+//       </button>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+
+
+// #Eight example of usestate
+// import React from 'react'
+// import { useState } from 'react'
+
+// function App() {
+//   const[val,setval]=useState([
+//     {name:"bibas",age:23},
+//     {name:"yamal", age:17},
+//     {name:"pedri", age:21}
+//   ])
+  
+  
+//   return (
+//     <div>
+//       {val.map((items)=>(
+//         <div>
+//         <h1>{items.name}</h1>
+//         <h2>{items.age}</h2>
+
+//         </div>
+//       ))}
+//       <button onClick={()=>setval(()=>val.map(items=>items.name==="yamal"? ({name:"yamal",age:items.age+1}):items))} className='px-2 py-1 rounded-md bg-blue-200 '> click</button>
+
+//     </div>
+//   )
+// }
+
+// export default App
+
+// Above example are the example of usestate done in App.jsx 
+
+
+
+// Now this example of usestate is done in card.jsx which is import from card.jsx
+
+// import React from 'react'
+// import Card from './Components/Card'
+// function App() {
+//   return (
+//     <>
+//       <Card/>
+//       <Card/>
+
+//     </>
+//   )
+// }
+
+// export default App
+
+
+
+// #LEARNING PASSING DATA VIA PROPS
+
+// # EXAMPLE 1
+
+// import React from 'react'
+// import Card from './Components/Card'
+
+// function App() {
+//   return (
+//     <div>
+//       <Card text="know more" color="bg-blue-500" />
+//       <Card text="download now" color="bg-red-600" />
+
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+
+
+
+// # EXAMPLE 2
+
+import React, { useState } from 'react'
+import Card from './Components/Card'
+
+function App() {
+
+
+  const data =[
+    {name:"bibas",profession:"coder", image:"https://images.unsplash.com/photo-1492288991661-058aa541ff43?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fHBvdHJhaXQlMjBtYWxlfGVufDB8fDB8fHww,", friends:false },
+    {name:"yamal",profession:"footballer", image:"https://images.unsplash.com/photo-1558487661-9d4f01e2ad64?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHBvdHJhaXQlMjBtYWxlfGVufDB8fDB8fHww",friends:false },
+    {name:"pedri",profession:"forestry", image:"https://images.unsplash.com/photo-1519058082700-08a0b56da9b4?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjd8fHBvdHJhaXQlMjBtYWxlfGVufDB8fDB8fHww",friends:false },
+    {name:"cubarsi",profession:"doctor", image:"https://images.unsplash.com/photo-1441786485319-5e0f0c092803?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzF8fHBvdHJhaXQlMjBtYWxlfGVufDB8fDB8fHww",friends:false }
+
+  ];
+
+  const [realdata, setrealdata]=useState(data)
+  const handlefriendsbutton=(cardindex)=>{
+    setrealdata ((previous)=>{
+      return previous.map((items,index)=>{
+        if (index===cardindex){
+          return{...items,friends: !items.friends}
+        }
+        return items;
+      })
+    })
+  }
   return (
-    <div className="px-5 py-5">
-      {/* <Card /> */}
-      <h1>{banned.toString()}</h1>
-      <button onClick={()=>{setbanned(!banned)}} className="px-2 py-1 bg-blue-700 rounded-full mt-2">change the score</button>
+    
+    <>
+    <div className='w-full h-screen bg-zinc-200 gap-4 flex justify-center items-center'>
+
+    {realdata.map((items,index)=>(
+      <Card key={index} index={index} handleclick={handlefriendsbutton} values={items} />
+    ))}
+    
     </div>
     
+
+    </>
+
+   
   )
 }
 
-export default App;
+export default App
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,29 +1,114 @@
-import React from 'react'
+// import React from 'react'
 
-function Card() {
+// function Card() {
 
-  const data=[
-    {name:"diamond and Rust", description:"This song is sang by Joan baez in 1973"},
-    {name:"November rain", description:"This song is sang by Guns and Roses in 1997"},
+//   const data=[
+//     {name:"diamond and Rust", description:"This song is sang by Joan baez in 1973"},
+//     {name:"November rain", description:"This song is sang by Guns and Roses in 1997"},
    
 
-  ]
-    const handledownload=()=>{alert("you can't download")}
+//   ]
+//     const handledownload=()=>{alert("you can't download")}
 
-  return (
+//   return (
     
-      <div className='w-full h-screen bg-zinc-300 justify-center items-center flex flex-col gap-4'>
-        {data.map((items, index)=><div className="w-100 song px-3 py-2 bg-zinc-100 rounded-md">
-          <h3 className='font-semibold text-xl'>{items.name}</h3>
-          <p className='text-xs mt-2'>{items.description}</p>
-          <button onClick={handledownload} className='px-2 py-1 bg-blue-400 text-xs font-semibold text-zinc-800 rounded-md mt-3'> Download</button>
-        </div>)}
+//       <div className='w-full h-screen bg-zinc-300 justify-center items-center flex flex-col gap-4'>
+//         {data.map((items, index)=><div className="w-100 song px-3 py-2 bg-zinc-100 rounded-md">
+//           <h3 className='font-semibold text-xl'>{items.name}</h3>
+//           <p className='text-xs mt-2'>{items.description}</p>
+//           <button onClick={handledownload} className='px-2 py-1 bg-blue-400 text-xs font-semibold text-zinc-800 rounded-md mt-3'> Download</button>
+//         </div>)}
         
-      </div>
+//       </div>
     
       
+//   )
+// }
+
+// export default Card
+
+
+// above example are the example of buidling components
+
+
+
+// This below example are the example of the usestate
+// import React, { useState } from 'react'
+
+// function Card() {
+  
+//   const [val ,setval]=useState(false);
+  
+//   return (
+//     <div className='p-5'>
+//       <h1>
+//      {val===false?"bahar jao":"matjao"}
+//       </h1>
+//       <button onClick={()=>setval(()=>!val)} className='px-2 py-1 bg-blue-500 rounded-md'> click</button>
+     
+//     </div>
+//   )
+// }
+
+// export default Card
+
+
+
+// import React, { useState } from 'react'
+// import { FaArrowRight } from "react-icons/fa";
+
+
+
+
+// function Card() {
+
+//   const [val,setval]=useState(false)
+//   return (
+//     <div className='w-full h-screen bg-zinc-800 flex justify-center items-center'>
+//     <div className=' relative w-80 h-64 bg-zinc-200 flex rounded-md overflow-hidden '>
+//       <img className={`shrink-0 w-full transition-transform duration-700 ease-in-out ${val===false ?'-translate-x-[0%]':'-translate-x-[100%]'} h-full object-cover`} src="https://images.unsplash.com/photo-1741851373816-91017faa8553?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+//       <img className={`shrink-0 w-full transition-transform duration-700 ease-in-out  ${val===false ?'-translate-x-[0%]':'-translate-x-[100%]'} h-full object-cover`} src="https://plus.unsplash.com/premium_photo-1712685912274-2483dade540f?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDEzfDZzTVZqVExTa2VRfHxlbnwwfHx8fHw%3D" alt="" />
+//       <span onClick={()=>setval(()=>!val)} className='w-10 h-10 bg-zinc-200 absolute bottom-[0%] flex justify-center items-center bg-[#dadada7b] rounded-full left-1/2 -translate-x-[50%] -translate-y-[50%]'>
+//       <FaArrowRight />
+//       </span>
+      
+
+
+//     </div>
+      
+//     </div>
+//   )
+// }
+
+// export default Card
+
+
+
+
+
+
+// learning Props section
+
+
+import React from 'react'
+
+function Card({values,handleclick,index}) {
+  const {image,name,profession,friends}=values;
+  return (
+    <div className='w-52 bg-white rounded-md overflow-hidden'>
+      <div className='w-full h-40 bg-sky-200'>
+        <img className='w-full h-full object-cover object-[center_top]' src={image} alt="" />
+      </div>
+      <div className='w-full p-3'>
+        <h3 className='text-xl font-semibold'>{name}</h3>
+        <h5 className='text-xs'>{profession}</h5>
+        <button onClick={()=>handleclick(index)} className='px-3 py-1 text-xs text-white bg-blue-500 font-semibold rounded-md'>{friends===true ? "friends":"Add friends"}</button>
+      </div>
+    </div>
+  
+      
+    
   )
 }
 
 export default Card
-
